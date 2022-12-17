2cca3f2b2d394733a20c4e6ae89395e0c323e9ce
harper25 <olesjakubb@gmail.com>
2022-12-17 10:19:16 +0100

    Add temp argparse debug script

---
+++ tool.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-d", # change shortcut? how is it used in different libs to indicate path?
+    "--dir",
+    nargs="?",
+    help="directory with your git repositories, defaults to the current directory",
+    default=os.getcwd(),
+)
+parser.add_argument(
+    "-r", "--reg", help="regex for filtering repositories to show", default=None
+)
+subparsers = parser.add_subparsers(dest='command', help='sub-command help')
+
+parser_status = subparsers.add_parser('status', help='status')
+
+parser_pull = subparsers.add_parser('pull', help='a help')
+
+parser_checkout = subparsers.add_parser('checkout', help='b help')
+parser_checkout.add_argument('branch', help='baz help')
+
+input_args = []
+args0 = parser.parse_args(input_args) # should be status
+print(input_args)
+print(args0)
+
+input_args = ["--dir", "/root"]
+args1 = parser.parse_args(input_args) # should be status
+print(input_args)
+print(args1)
+
+input_args = ["checkout", "dev"]
+args2 = parser.parse_args(input_args)
+print(input_args)
+print(args2)
+
+input_args = ["pull"]
+args3 = parser.parse_args(input_args)
+print(input_args)
+print(args3)
+
+input_args = ["status"]
+args4 = parser.parse_args(input_args)
+print(input_args)
+print(args4)
+
